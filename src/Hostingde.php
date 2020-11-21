ffb4cecccde98bf8808bf53a5be7268460fab824
KuLi <KuLi@users.noreply.github.com>
2020-11-21 13:25:58 +0100

    initial commit

---
+++ src/Hostingde.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace acme\hostingde;
+
+use acme\common\Client;
+
+class Hostingde extends Client
+{
+    public function present(string $fqdn, string $txt)
+    {
+        // TODO: Implement present() method.
+    }
+
+    public function cleanUp(string $fqdn, string $txt)
+    {
+        // TODO: Implement cleanUp() method.
+    }
+}
